ee8baadcbeeaf4e843e0851ba0e22735214129f4
Oliver Forral <Oliver.Forral@gmail.com>
2021-01-15 14:05:52 -0800

    The DocumentEngine class

--- Chaper01/eBookManager/eBookManager.Engine/DocumentEngine.cs
+++ Chaper01/eBookManager/eBookManager.Engine/DocumentEngine.cs
@@ -1,7 +1,43 @@
-﻿namespace eBookManager.Engine
+﻿using System;
+using System.IO;
+
+namespace eBookManager.Engine
 {
-    class DocumentEngine
+    public class DocumentEngine
     {
+        public (
+            DateTime created,
+            DateTime dateLastAccessed,
+            string fileName,
+            string extension,
+            long fileSize,
+            bool error) GetFileProperties(string filePath)
+        {
+            var returnTuple = (
+                created: DateTime.MinValue,
+                lastAccessed: DateTime.MinValue,
+                fileName: "",
+                extension: "",
+                fileSize: 0L,
+                error: false);
+            try
+            {
+                FileInfo fi = new FileInfo(filePath);
+                fi.Refresh();
+                returnTuple = (
+                    fi.CreationTime,
+                    fi.LastAccessTime,
+                    fi.Name,
+                    fi.Extension,
+                    fi.Length,
+                    false);
+            }
+            catch
+            {
+                returnTuple.error = true;
+            }
+            return returnTuple;
+        }
     }
 }
 
